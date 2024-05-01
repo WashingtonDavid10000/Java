6602a5e43506ddecfdddc90712455c5eb4640658
Washington David <Washington.David4512@gmail.com>
2024-05-01 11:53:58 -0300

    Finalizando sintaxes báiscas do java

---
+++ Estudos_Java/Dio/Basico/Java_Terminal/src/Comentarios.java
@@ -0,0 +1,15 @@
+public class Comentarios {
+    public static void main(String[] args) {
+
+        //Comentário de uma linha...
+
+        /*
+         * Comentário de múltiplas linhas...
+         */
+
+        /**
+         * Comentário para documentação...
+         */
+ 
+    }
+}
---
+++ Estudos_Java/Dio/Basico/Java_Terminal/src/SobreMin.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SobreMin {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome + ".");
+        System.out.println("Tenho " + idade + " anos de idade.");
+        System.out.println("Minha altura é " + altura + "cm.");
+    }
+}
